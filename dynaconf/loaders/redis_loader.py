c38d268d7a81b3ffab85414cf9d8be78104afeea
Bruno Rocha <rochacbruno@gmail.com>
2015-12-22 20:24:35 -0200

    redis loader uses hash

--- dynaconf/loaders/redis_loader.py
+++ dynaconf/loaders/redis_loader.py
@@ -4,17 +4,24 @@ from dynaconf.utils.parse_conf import parse_conf_data
 IDENTIFIER = 'redis_loader'
 
 
-def load(obj, namespace=None, silent=True):
+def load(obj, namespace=None, silent=True, key=None):
     redis = StrictRedis(**obj.REDIS_FOR_DYNACONF)
     namespace = namespace or obj.DYNACONF_NAMESPACE
+    holder = "DYNACONF_%s" % namespace
     try:
-        data = {
-            key.partition('_')[-1]: parse_conf_data(redis.get(key))
-            for key in redis.keys('%s_*' % namespace)
-        }
-        obj.loaded_by_loaders[IDENTIFIER] = data
-        for key, value in data.items():
-            obj.set(key, value)
+        if key:
+            value = parse_conf_data(redis.hget(holder, key))
+            if value:
+                obj.set(key, value)
+        else:
+            data = {
+                key: parse_conf_data(value)
+                for key, value in redis.hgetall(holder).items()
+            }
+            if data:
+                obj.loaded_by_loaders[IDENTIFIER] = data
+                for key, value in data.items():
+                    obj.set(key, value)
     except Exception as e:
         e.message = 'Unable to load config from redis (%s)' % e.message
         if silent:
