40dc50284b4e158e49a787341b0f865544075af8
Bruno Rocha <rochacbruno@gmail.com>
2018-05-12 22:43:05 -0300

    Fixing for python-box 3.2.0

--- dynaconf/utils/boxing.py
+++ dynaconf/utils/boxing.py
@@ -5,25 +5,25 @@ from box import Box
 
 class DynaBox(Box):
     """Specialized Box for dynaconf
-    it allows items/attrs to be founf both in upper or lower case"""
+    it allows items/attrs to be found both in upper or lower case"""
 
-    def __getattr__(self, item):
+    def __getattr__(self, item, *args, **kwargs):
         try:
-            return super(DynaBox, self).__getattr__(item)
+            return super(DynaBox, self).__getattr__(item, *args, **kwargs)
         except (AttributeError, KeyError):
             n_item = item.lower() if item.isupper() else item.upper()
-            return super(DynaBox, self).__getattr__(n_item)
+            return super(DynaBox, self).__getattr__(n_item, *args, **kwargs)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item, *args, **kwargs):
         try:
-            return super(DynaBox, self).__getitem__(item)
+            return super(DynaBox, self).__getitem__(item, *args, **kwargs)
         except (AttributeError, KeyError):
             n_item = item.lower() if item.isupper() else item.upper()
-            return super(DynaBox, self).__getitem__(n_item)
+            return super(DynaBox, self).__getitem__(n_item, *args, **kwargs)
 
-    def get(self, item, default=None):
-        value = super(DynaBox, self).get(item, default)
+    def get(self, item, default=None, *args, **kwargs):
+        value = super(DynaBox, self).get(item, default, *args, **kwargs)
         if value is None or value == default:
             n_item = item.lower() if item.isupper() else item.upper()
-            value = super(DynaBox, self).get(n_item, default)
+            value = super(DynaBox, self).get(n_item, default, *args, **kwargs)
         return value
