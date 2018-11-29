24835d636369a1945cf81c937c966eede56261b5
mspinelli <mspinelli.software@gmail.com>
2018-11-29 10:51:00 -0500

    fixed infinite recursion caused by copy()

--- dynaconf/utils/boxing.py
+++ dynaconf/utils/boxing.py
@@ -21,6 +21,12 @@ class DynaBox(Box):
             n_item = item.lower() if item.isupper() else item.upper()
             return super(DynaBox, self).__getitem__(n_item, *args, **kwargs)
 
+    def __copy__():
+        return self.__class__(super(Box, self).copy())
+
+    def copy():
+        return self.__class__(super(Box, self).copy())
+
     def get(self, item, default=None, *args, **kwargs):
         value = super(DynaBox, self).get(item, default, *args, **kwargs)
         if value is None or value == default:
