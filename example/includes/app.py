7b4381a276ea9831d140af4442bb3bb5fd0ba581
Bruno Rocha <rochacbruno@gmail.com>
2019-03-27 12:03:22 -0300

    Add dynaconf_include examples

---
+++ example/includes/app.py
@@ -0,0 +1,16 @@
+from dynaconf import settings
+
+assert settings.DYNACONF_INCLUDE == ['configs/*', 'this_is_not_loaded.toml']
+assert settings.SETTINGS_VAR is True
+assert settings.LASTFILE == 'configs/plugin4.py'
+assert settings.MERGEABLE == {
+    'plugin1': True,
+    'plugin2': True,
+    'plugin3': True,
+    'plugin4': True,
+    'settings': True
+}
+assert settings.PLUGIN1_VAR is True
+assert settings.PLUGIN2_VAR is True
+assert settings.PLUGIN3_VAR is True
+assert settings.PLUGIN4_VAR is True
---
+++ example/includes/configs/plugin4.py
@@ -0,0 +1,3 @@
+PLUGIN4_VAR = True
+LASTFILE = "configs/plugin4.py"
+MERGEABLE = {"plugin4": True}
