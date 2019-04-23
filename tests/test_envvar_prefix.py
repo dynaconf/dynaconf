c26ea4e6bfee42c067a9f4846bc61ee1f4dbbe4c
Jan Willhaus <mail@janwillhaus.de>
2019-04-23 12:20:18 +0200

    Additional testcase for prefix being false from envvar

--- tests/test_envvar_prefix.py
+++ tests/test_envvar_prefix.py
@@ -1,10 +1,15 @@
 import os
 
+import pytest
+
 from dynaconf import LazySettings
 
 TOML = """
 [global]
 var = "my value"
+
+[false]
+thisvar = "should not be set"
 """
 
 
@@ -14,8 +19,24 @@ def test_envvar_prefix_lazysettings(tmpdir):
     tmpfile.write(TOML)
 
     settings = LazySettings(
-        ENVVAR_PREFIX_FOR_DYNACONF=False, SETTINGS_FILE_FOR_DYNACONF=str(tmpfile)
+        ENVVAR_PREFIX_FOR_DYNACONF=False,
+        SETTINGS_FILE_FOR_DYNACONF=str(tmpfile),
     )
 
     assert settings.VAR == "my value"
     assert settings.DYNACONF_PREFIXED_VAR == "this is prefixed"
+
+
+def test_envvar_prefix_false_from_envvar(tmpdir):
+    os.environ["DYNACONF_PREFIXED_VAR"] = "this is prefixed"
+    os.environ["ENVVAR_PREFIX_FOR_DYNACONF"] = "false"
+    tmpfile = tmpdir.mkdir("sub").join("test_no_envvar_prefix.toml")
+    tmpfile.write(TOML)
+
+    settings = LazySettings(SETTINGS_FILE_FOR_DYNACONF=str(tmpfile))
+
+    assert settings.VAR == "my value"
+    assert settings.DYNACONF_PREFIXED_VAR == "this is prefixed"
+
+    with pytest.raises(AttributeError):
+        assert settings.THISVAR == "should not be set"
