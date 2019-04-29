812ab40215d8e87b57879ad00bcf716bb707c1b4
Jan Willhaus <mail@janwillhaus.de>
2019-04-29 17:05:47 +0200

    Use parametrized test instead of repeating code

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -1,50 +1,32 @@
 import os
 
+import pytest
+
 from dynaconf import LazySettings
 
 
-def test_dynaconf_namespace_renamed(tmpdir, recwarn):
-    params = dict(
-        DYNACONF_NAMESPACE="FOO",
-        DYNACONF_SETTINGS_MODULE="foo.py",
-        SETTINGS_MODULE="foo.py",
-        PROJECT_ROOT=str(tmpdir),
-        DYNACONF_SILENT_ERRORS=True,
-        DYNACONF_ALWAYS_FRESH_VARS=["BAR"],
-    )
-    settings = LazySettings(**params)
+@pytest.mark.parametrize(
+    "deprecated,value,new",
+    [
+        ("DYNACONF_NAMESPACE", "FOO", "ENV_FOR_DYNACONF"),
+        ("NAMESPACE_FOR_DYNACONF", "FOO", "ENV_FOR_DYNACONF"),
+        ("DYNACONF_SETTINGS_MODULE", "foo.py", "SETTINGS_FILE_FOR_DYNACONF"),
+        ("SETTINGS_MODULE", "foo.py", "SETTINGS_FILE_FOR_DYNACONF"),
+        ("PROJECT_ROOT", "./", "ROOT_PATH_FOR_DYNACONF"),
+        ("DYNACONF_SILENT_ERRORS", True, "SILENT_ERRORS_FOR_DYNACONF"),
+        ("DYNACONF_ALWAYS_FRESH_VARS", ["BAR"], "FRESH_VARS_FOR_DYNACONF"),
+        ("GLOBAL_ENV_FOR_DYNACONF", "MYAPP", "ENVVAR_PREFIX_FOR_DYNACONF"),
+        ("BASE_NAMESPACE_FOR_DYNACONF", "THIS", "DEFAULT_ENV_FOR_DYNACONF"),
+    ],
+)
+def test_dynaconf_namespace_renamed(tmpdir, recwarn, deprecated, value, new):
+    settings = LazySettings(**{deprecated: value})
 
-    for param, warning in zip(params.keys(), recwarn):
-        assert issubclass(warning.category, DeprecationWarning)
-        assert param in str(warning.message)
-
-    assert settings.ENV_FOR_DYNACONF == "FOO"
-    assert settings.SETTINGS_FILE_FOR_DYNACONF == "foo.py"
-    assert settings.ROOT_PATH_FOR_DYNACONF == str(tmpdir)
-    assert settings.SILENT_ERRORS_FOR_DYNACONF is True
-    assert settings.FRESH_VARS_FOR_DYNACONF == ["BAR"]
-
-
-def test_namespace_for_dynaconf_renamed(tmpdir, recwarn):
-    params = dict(
-        NAMESPACE_FOR_DYNACONF="FOO",
-        DYNACONF_SETTINGS_MODULE="foo.py",
-        SETTINGS_MODULE="foo.py",
-        PROJECT_ROOT_FOR_DYNACONF=str(tmpdir),
-        DYNACONF_SILENT_ERRORS=True,
-        DYNACONF_ALWAYS_FRESH_VARS=["BAR"],
-    )
-    settings = LazySettings(**params)
-
-    for param, warning in zip(params.keys(), recwarn):
-        assert issubclass(warning.category, DeprecationWarning)
-        assert param in str(warning.message)
-
-    assert settings.ENV_FOR_DYNACONF == "FOO"
-    assert settings.SETTINGS_FILE_FOR_DYNACONF == "foo.py"
-    assert settings.ROOT_PATH_FOR_DYNACONF == str(tmpdir)
-    assert settings.SILENT_ERRORS_FOR_DYNACONF is True
-    assert settings.FRESH_VARS_FOR_DYNACONF == ["BAR"]
+    assert len(recwarn) == 1
+    assert issubclass(recwarn[0].category, DeprecationWarning)
+    assert deprecated in str(recwarn[0].message)
+    assert new in str(recwarn[0].message)
+    assert settings.get(new) == value
 
 
 def test_envvar_prefix_for_dynaconf(tmpdir, recwarn):
