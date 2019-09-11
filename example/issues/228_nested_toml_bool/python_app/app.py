8ed0995c7f787a6474d4cf2a4f12257df3f5c3dd
Bruno Rocha <rochacbruno@users.noreply.github.com>
2019-09-11 12:28:16 -0300

    Fix #228 add `ignore` argument to Django explicit mode. (#229)

---
+++ example/issues/228_nested_toml_bool/python_app/app.py
@@ -0,0 +1,12 @@
+from dynaconf import settings
+
+print(settings.DEBUG.enabled)
+assert settings.DEBUG.enabled is False
+
+print(settings.get("debug.enabled"))
+assert settings.get("debug.enabled") is False
+
+
+DEBUG = settings.get("debug.enabled", False)
+print(DEBUG)
+assert DEBUG is False
