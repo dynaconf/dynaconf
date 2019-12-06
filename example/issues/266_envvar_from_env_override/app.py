d4c86b9e9f2983c4a48b4942542c614393517347
Bruno Rocha <rochacbruno@gmail.com>
2019-12-06 17:51:44 -0200

    Fix #266 created new variable FORCE_ENV to override ENV_FOR_DYNACONF

---
+++ example/issues/266_envvar_from_env_override/app.py
@@ -0,0 +1,10 @@
+from dynaconf import settings
+
+print(f"Current env: {settings.current_env}")
+
+print(f"Dev is: {settings.THISIS}")
+
+print(f"Production is: {settings.from_env('production').THISIS}")
+
+assert settings.THISIS == "development"
+assert settings.from_env("production").THISIS == "production"
