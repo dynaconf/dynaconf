2783c92f586c8fcfb27b1598c929ab27b5e13d3f
Bruno Rocha <rochacbruno@gmail.com>
2020-01-24 14:03:00 -0200

    Overriding strategy test

---
+++ example/overriding/app.py
@@ -0,0 +1,9 @@
+from dynaconf import settings
+
+
+assert settings.REST_FRAMEWORK.DEFAULT_AUTHENTICATION_CLASSES == [
+    "rest_framework.authentication.SessionAuthentication",
+    # user_settings.py will remove the line below
+    # 'pulpcore.app.authentication.PulpRemoteUserAuthentication',
+    "rest_framework.authentication.BasicAuthentication",
+], settings.REST_FRAMEWORK.DEFAULT_AUTHENTICATION_CLASSES
---
+++ example/overriding/user_settings.py
@@ -0,0 +1,28 @@
+# Alternative 1
+# This works
+# https://dynaconf.readthedocs.io/en/latest/guides/environment_variables.html#merging-new-data-to-existing-variables
+
+REST_FRAMEWORK__DEFAULT_AUTHENTICATION_CLASSES = "@reset ['rest_framework.authentication.SessionAuthentication','rest_framework.authentication.BasicAuthentication']"  # noqa
+
+
+# Alternative 2 (NOT IMPLEMENTED YET)
+# TODO: Implement this merging override strategy
+
+_REST_FRAMEWORK = {
+    # Mark the `REST_FRAMEWORK` as a candidate for merging
+    # specify the merge strategy for specific keys
+    # strategies must be:
+    # reset: clean existing value before reassign
+    # merge: (the default) merge with existing values
+    # unique: merge only if value doesn't already exists in the collection
+    # conditional(expr): expression to evaluate as a conditional
+    "dynaconf_merge": {
+        "DEFAULT_AUTHENTICATION_CLASSES": {"strategy": "reset"}
+        # ^ the key will be cleaned before assign new values
+    },
+    # Set keys you want to assign
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ),
+}
