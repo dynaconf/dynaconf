03ea15f66ea5fc7f41919552f6b14451c034bce3
Douglas Maciel d'Auriol Souza <ddauriol@gmail.com>
2020-04-11 18:42:19 -0300

    Checked: Flake8

--- tests/test_validators_conditions.py
+++ tests/test_validators_conditions.py
@@ -14,7 +14,7 @@ positive_conditions = [
     ("is_type_of", 42, int),
     ("is_in", 42, [42, 34]),
     ("is_not_in", 42, [55, 34]),
-    ("contd", "This word(s) contains in text", "in" ),
+    ("contd", "This word(s) contains in text", "in"),
     ("len_eq", "Length Equal", 12),
     ("len_ne", "Length Not equal", 0),
     ("len_min", "Minimum length", 3),
