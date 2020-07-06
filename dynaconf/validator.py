6579a6aac6536e3859a62fed7f32a5a6bbac8579
Piotr Baniukiewicz <baniuk1@gmail.com>
2020-07-06 15:41:55 +0200

    Fix validation of optional fields (#370)
    * Fix validation of optional fields
    
    * More concise code
    
    Co-authored-by: Bruno Rocha <rochacbruno@users.noreply.github.com>
    
    Co-authored-by: Bruno Rocha <rochacbruno@users.noreply.github.com>

--- dynaconf/validator.py
+++ dynaconf/validator.py
@@ -182,7 +182,7 @@ class Validator(object):
                         name=name, env=env
                     )
                 )
-            elif self.must_exist is False and not exists:
+            elif self.must_exist in (False, None) and not exists:
                 return
 
             value = self.cast(settings.get(name, empty))
