60998301aeabed89f5ea7e7a3f01cd520fc63f65
Bruno Rocha <rochacbruno@gmail.com>
2020-08-06 14:29:41 -0300

    Fix #379 add missing __iter__ and items

---
+++ example/issues/379_dict_like_iteration/app.py
@@ -0,0 +1,17 @@
+from dynaconf import Dynaconf
+
+settings = Dynaconf(settings_files=["settings.toml"],)
+
+assert settings.key == "value"
+assert settings.number == 789
+assert settings.a_dict.nested.other_level == "nested value"
+assert settings["a_boolean"] is False
+assert settings.get("DONTEXIST", default=1) == 1
+
+
+for item in settings:
+    print(item)
+
+
+for key, value in settings.items():  # dict like iteration
+    print(key, value)
