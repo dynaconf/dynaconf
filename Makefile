684fca3a155209673c5ce779e90be1823b01a7c9
Bruno Rocha <rochacbruno@users.noreply.github.com>
2020-10-01 01:04:07 -0300

    Fix #434 setenv failing to unset LazyValues (#437)
    Fix #434

--- Makefile
+++ Makefile
@@ -103,6 +103,8 @@ test_examples:
 	cd example/issues/384_dotted_set;pwd;python app.py
 	cd example/issues/392_evaluate_nested_structures;pwd;DYNACONF_INITIAL='@merge [1,2,3]' python app.py
 	cd example/issues/404_dup_validator_message;pwd;python app.py
+	cd example/issues/434_setenv;pwd;python app.py --config development dynaconf
+
 test_vault:
 	# @cd example/vault;pwd;python write.py
 	docker run --rm --name dynaconf_with_vault -d -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' -p 8200:8200 vault
---
+++ example/issues/434_setenv/app.py
@@ -0,0 +1,28 @@
+import click
+
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    envvar_prefix="DYNACONF",
+    settings_files=["settings.toml", ".secrets.toml"],
+    environments=True,
+)
+
+
+@click.group()
+@click.option("--config", default="default")
+def main(config):
+    click.echo(f"Environment: {config}")
+    settings.setenv(config)
+    return
+
+
+@main.command()
+def dynaconf():
+    click.echo(f"Database Path: {settings.DB_PATH}")
+    assert "/mydb.db" in settings.DB_PATH
+    return
+
+
+if __name__ == "__main__":
+    main()
