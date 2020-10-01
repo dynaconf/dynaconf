2a69d07b10fb575611267b813805c8361d6a2a25
Bruno Rocha <rochacbruno@users.noreply.github.com>
2020-10-01 16:33:20 -0300

    Close #284 not a bug (#440)
    284 is not a bug but a missing of explicit merge tokens

---
+++ example/issues/284_local_sets/app.py
@@ -0,0 +1,12 @@
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    settings_file="settings.toml",
+    environments=True,
+    # merge_enabled=True  # this enabled global merge of all files
+)
+
+print(settings.A)
+
+assert settings.get("A").get("B").get("C") == "1"
+assert settings.get("A").get("cousin").get("D") == "2"
