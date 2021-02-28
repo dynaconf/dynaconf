5cdc20f77d48312104df0b8a71b3493dd059fd57
Bruno Rocha <rochacbruno@users.noreply.github.com>
2021-02-28 19:03:54 -0300

    Fix #482 - formatter case insensitive (#527)
    * Fix #482 - formatter using both upper and lowercase access
    
    Fix #482
    
    * add more testes covering nested formatting

--- dynaconf/base.py
+++ dynaconf/base.py
@@ -281,6 +281,28 @@ class Settings:
         """Respond to `item in settings`"""
         return item.upper() in self.store or item.lower() in self.store
 
+    def __getattr__(self, item):
+        """This method is called only when the attribute does not exits
+        usually on access to the direct `Settings` object instead of the
+        `LazySettings`.
+        All first level attributes are stored as UPPERCASE, so self.FOO
+        will always be accessible, however in some cases `self.foo` might
+        be acessible. This method routes this access to the underlying `_store`
+        which handles casing and recursive access.
+        """
+        try:
+            if (
+                item.islower()
+                and self._store.get("LOWERCASE_READ_FOR_DYNACONF", empty)
+                is False
+            ):
+                raise KeyError
+            value = self._store[item]
+        except KeyError:
+            raise AttributeError(f"Settings has no attribute '{item}'")
+        else:
+            return value
+
     def __getitem__(self, item):
         """Allow getting variables as dict keys `settings['KEY']`"""
         value = self.get(item, default=empty)
