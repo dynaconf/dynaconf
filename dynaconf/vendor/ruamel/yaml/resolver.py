0273d3056f99caaca9573df6b31ad1d8319b4073
Bruno Rocha <rochacbruno@users.noreply.github.com>
2021-03-02 17:49:31 -0300

    Fix #462 make DynaBox nested List to use DynaBox as default class (#533)
    Fix #462

--- dynaconf/vendor/ruamel/yaml/resolver.py
+++ dynaconf/vendor/ruamel/yaml/resolver.py
@@ -61,7 +61,7 @@ class BaseResolver:
 		elif kind is list:kind=SequenceNode
 		elif kind is dict:kind=MappingNode
 		elif kind not in[ScalarNode,SequenceNode,MappingNode]and kind is not _A:raise ResolverError('Invalid node kind: %s'%(kind,))
-		cls.yaml_path_resolvers[(tuple(new_path),kind)]=tag
+		cls.yaml_path_resolvers[tuple(new_path),kind]=tag
 	def descend_resolver(self,current_node,current_index):
 		if not self.yaml_path_resolvers:return
 		exact_paths={};prefix_paths=[]
@@ -70,10 +70,10 @@ class BaseResolver:
 			for (path,kind) in self.resolver_prefix_paths[-1]:
 				if self.check_resolver_prefix(depth,path,kind,current_node,current_index):
 					if len(path)>depth:prefix_paths.append((path,kind))
-					else:exact_paths[kind]=self.yaml_path_resolvers[(path,kind)]
+					else:exact_paths[kind]=self.yaml_path_resolvers[path,kind]
 		else:
 			for (path,kind) in self.yaml_path_resolvers:
-				if not path:exact_paths[kind]=self.yaml_path_resolvers[(path,kind)]
+				if not path:exact_paths[kind]=self.yaml_path_resolvers[path,kind]
 				else:prefix_paths.append((path,kind))
 		self.resolver_exact_paths.append(exact_paths);self.resolver_prefix_paths.append(prefix_paths)
 	def ascend_resolver(self):
