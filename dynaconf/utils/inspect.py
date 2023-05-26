8c16da626670d5d70ede326fd288190f5e719f0e
pedro-psb <pedropsb95@gmail.com>
2023-05-26 17:58:45 -0300

    adjust affected parts of the system (catched by tests)

--- dynaconf/utils/inspect.py
+++ dynaconf/utils/inspect.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
-from textwrap import dedent, indent
-from typing import TYPE_CHECKING, Any, Callable
+from textwrap import dedent
+from textwrap import indent
+from typing import Any
+from typing import Callable
+from typing import TYPE_CHECKING
 
 from django.utils.version import sys
 from lark.utils import suppress
@@ -11,6 +14,18 @@ from dynaconf.vendor.ruamel import yaml
 if TYPE_CHECKING:
     from dynaconf import Dynaconf
     from dynaconf.loaders.base import SourceMetadata
+
+
+def inspect_key(setting, key_dotted_path: str):
+    """
+    Dumps loading history about a specific key (as dotted path)
+    """
+    print("[current-key-data]")
+    print(f"- key: {repr(key_dotted_path)}")
+    print(f"- value: {repr(setting.get(key_dotted_path))}")
+    print(f"- env: {repr(setting.current_env)}")
+    print("\n[loading-history]")
+    dump_data_by_source(setting, key_dotted_path)
 
 
 def dump_data_by_source(obj: Dynaconf, key_dotted_path: str = ""):
@@ -46,7 +61,11 @@ def dump_data_by_source(obj: Dynaconf, key_dotted_path: str = ""):
     for source_metadata, data in obj._loaded_by_loaders.items():
         order_count += 1
         try:
-            data = get_data_by_key(data, key_dotted_path) if key_dotted_path else data
+            data = (
+                get_data_by_key(data, key_dotted_path)
+                if key_dotted_path
+                else data
+            )
         except KeyError:
             # this source does not contain the requested key
             continue
@@ -58,7 +77,9 @@ def dump_data_by_source(obj: Dynaconf, key_dotted_path: str = ""):
         print(f"{pad}data:")
         # TODO docs don't recommended this. should indent streams directly
         print(
-            indent(yaml.dump(data, Dumper=yaml.RoundTripDumper), prefix=pad * 2),
+            indent(
+                yaml.dump(data, Dumper=yaml.RoundTripDumper), prefix=pad * 2
+            ),
             end="",
         )
 
@@ -77,22 +98,6 @@ def get_data_by_key(data: dict, key_dotted_path: str, default: Any = None):
                 data = data[int(node)]
     except (ValueError, IndexError, KeyError):
         if not default:
-            raise KeyError(f"Path not found in data")
+            raise KeyError(f"Path not found in data: {repr(key_dotted_path)}")
         return default
     return data
-
-
-def inspect_key(setting, key_dotted_path: str):
-    """
-    Dumps loading history about a specific key (as dotted path)
-
-    TODO:
-        - Add better error messages, reporting the type of the node
-          that caused the lookup error.
-    """
-    print(f"[current-key-data]")
-    print(f"- key: {repr(key_dotted_path)}")
-    print(f"- value: {repr(setting.get(key_dotted_path))}")
-    print(f"- env: {repr(setting.current_env)}")
-    print(f"\n[loading-history]")
-    dump_data_by_source(setting, key_dotted_path)
