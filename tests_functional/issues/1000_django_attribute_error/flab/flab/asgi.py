0dfdbe992c0c3fbbe0916326a3872c080057d064
Bruno Rocha <rochacbruno@gmail.com>
2023-09-11 11:39:15 +0100

    fix(django): fix #1000 AttributeError on admin
    fix #1000

---
+++ tests_functional/issues/1000_django_attribute_error/flab/flab/asgi.py
@@ -0,0 +1,17 @@
+"""
+ASGI config for flab project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+from __future__ import annotations
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flab.settings")
+
+application = get_asgi_application()
