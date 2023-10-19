7690aca0a64da5e3328e169338df1344c188b95d
pedropsb <pedropsb95@gmail.com>
2023-10-19 11:22:22 -0300

    fix(cli): support for 'entrypoint-path' in FLASK_APP #946

---
+++ tests_functional/issues/946_flask_cli/myapp.py
@@ -0,0 +1,13 @@
+"""
+CLI wouldnt recognize FLASK_APP "entrypoint" path (foo.bar:app)
+
+https://github.com/dynaconf/dynaconf/issues/946
+"""
+from __future__ import annotations
+
+from flask import Flask
+
+from dynaconf import FlaskDynaconf
+
+app = Flask(__name__)
+FlaskDynaconf(app)
