efafc8c7b93c39f04d00ea9f9e760355d07427ff
Pedro Brochado <pedropsb95@gmail.com>
2024-01-17 23:14:22 -0300

    Update script to create release commit

--- .github/scripts/create-release-commit.sh
+++ .github/scripts/create-release-commit.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+make setup-pre-commit
 NEW_VERSION="$1"
+OLD_VERSION="$(git describe --tags --abbrev=0)"
+echo "NEW_VERSION=$NEW_VERSION"
+echo "OLD_VERSION=$OLD_VERSION"
 
 # 1. Bump Version
-
 echo "${NEW_VERSION}" > dynaconf/VERSION
 sed -i "s/Dynaconf - [[:digit:]]*.[[:digit:]]*.[[:alnum:]]*/Dynaconf - ${NEW_VERSION}/" mkdocs.yml
-
-# 2. Generate Changelog
-git-changelog --bump "$NEW_VERSION" --in-place
-
-# 3. Create a new commit and annotated tag.
-git add dynaconf/VERSION mkdocs.yml CHANGELOG.md
-make setup-pre-commit
 pre-commit run --files dynaconf/VERSION mkdocs.yml || true
 
-commit_message="$(git shortlog "${old_version}.." | sed 's/^./    &/')"
+# 2. Generate Changelog
+git-changelog --bump "$NEW_VERSION" --in-place --filter-commits "3.2.4~.."
+
+# 3. Create a new commit
+git add dynaconf/VERSION mkdocs.yml CHANGELOG.md
+commit_message="$(git shortlog "${OLD_VERSION}.." | sed 's/^./    &/')"
 git commit \
-    --message "Release version ${new_version}" \
+    --message "Release version ${OLD_VERSION}" \
     --message "Shortlog of commits since last release:" \
     --message "${commit_message}"
-git tag --annotate "${new_version}" --message "Dynaconf ${new_version}" \
-    --message "${commit_message}"
+
+# 4. Add lightweight tag
+git tag "${NEW_VERSION}"
 
