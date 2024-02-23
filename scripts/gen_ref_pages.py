d222ce87eb3d0da3e30ce2c364f511f60768c313
mitches-got-glitches <mitch.edmunds@proton.me>
2024-02-23 09:45:43 +0000

    feat: Add mkdocstrings support with autogen API reference
    Followed the recipe on mkdocstrings docs to setup the autogeneration
    of API reference docs. Had to make some adjustments because this project
    does not have a src/ folder, just the folder with the project name.
    
    Tested results with mkdocs serve and everything builds. It takes ~80
    seconds to generate the whole API reference. Some exclusions may be
    worth introducing.

---
+++ scripts/gen_ref_pages.py
@@ -0,0 +1,36 @@
+"""Generate the code reference pages and navigation."""
+
+from pathlib import Path
+
+import mkdocs_gen_files
+
+nav = mkdocs_gen_files.Nav()
+
+root = Path(__file__).parent.parent
+src = Path(__file__).parent.parent / "dynaconf"
+docs_folder = root / "docs"
+
+for path in sorted(src.rglob("*.py")):
+    module_path = path.relative_to(root).with_suffix("")
+    doc_path = path.relative_to(root).with_suffix(".md")
+    full_doc_path = Path(docs_folder, "reference", doc_path)
+
+    parts = tuple(module_path.parts)
+
+    if parts[-1] == "__init__":
+        parts = parts[:-1]
+        doc_path = doc_path.with_name("index.md")
+        full_doc_path = full_doc_path.with_name("index.md")
+    elif parts[-1] == "__main__":
+        continue
+
+    nav[parts] = doc_path.as_posix()
+
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        identifier = ".".join(parts)
+        print("::: " + identifier, file=fd)
+
+    mkdocs_gen_files.set_edit_path(full_doc_path, Path("../") / path)
+
+with mkdocs_gen_files.open(Path(docs_folder, "reference/SUMMARY.md"), "w") as nav_file:
+    nav_file.writelines(nav.build_literate_nav())
