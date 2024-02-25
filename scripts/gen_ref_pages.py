b8365aa9f73ab0fd7000e6478a0dbbe6d7160d14
mitches-got-glitches <mitch.edmunds@proton.me>
2024-02-25 12:53:08 +0000

    fix: Pre-commit formatting errors

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -1,15 +1,18 @@
 """Generate the code reference pages and navigation.
 
-From the mkdocstrings recipe: https://mkdocstrings.github.io/recipes/#automatic-code-reference-pages
+From the mkdocstrings recipe:
+https://mkdocstrings.github.io/recipes/#automatic-code-reference-pages
 """
+from __future__ import annotations
+
 from pathlib import Path
 
 import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-# The template for this script made the assumption that the package files lived in
-# a src/ folder, so some of the paths here needed to change.
+# The template for this script made the assumption that the package files lived
+# in a src/ folder, so some of the paths here needed to change.
 root = Path(__file__).parent.parent
 src = Path(__file__).parent.parent / "dynaconf"
 
@@ -18,8 +21,8 @@ for path in sorted(src.rglob("*.py")):
     module_path = path.relative_to(root).with_suffix("")
     doc_path = path.relative_to(root).with_suffix(".md")
     # Needed to make sure the path here is relative to this file because
-    # mkdocs_gen_files defaulted to creating things in the temp folder for some reason.
-    # Caused git errors because files were outside the repo.
+    # mkdocs_gen_files defaulted to creating things in the temp folder for some
+    # reason. Caused git errors because files were outside the repo.
     full_doc_path = Path(root, "docs", "reference", doc_path)
 
     parts = tuple(module_path.parts)
@@ -37,11 +40,13 @@ for path in sorted(src.rglob("*.py")):
         identifier = ".".join(parts)
         print("::: " + identifier, file=fd)
 
-    # Followed the advice in the note in this section of the mkdocstrings recipe:
-    # https://mkdocstrings.github.io/recipes/#generate-pages-on-the-fly
+    # Followed the advice in the note in this section of the mkdocstrings
+    # recipe: https://mkdocstrings.github.io/recipes/#generate-pages-on-the-fly
     # Not doing this resulted in a 404 error.
     mkdocs_gen_files.set_edit_path(full_doc_path, Path("../") / path)
 
 # Similar comment to above, needed to make sure the path was relative.
-with mkdocs_gen_files.open(Path(root, "docs", "reference/SUMMARY.md"), "w") as nav_file:
+with mkdocs_gen_files.open(
+    Path(root, "docs", "reference/SUMMARY.md"), "w"
+) as nav_file:
     nav_file.writelines(nav.build_literate_nav())
