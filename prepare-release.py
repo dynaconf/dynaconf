6e1a4e4e1bc2366285f33b878d607cf639bfafbe
Pedro Brochado <pedropsb95@gmail.com>
2024-03-16 15:38:24 -0300

    Fix version files manually

---
+++ prepare-release.py
@@ -0,0 +1,17 @@
+#!/bin/python
+#
+# This prepares for the release by:
+# - bumping version files to the final release version (e.g, x.y.z without .dev)
+# - updating the changelog
+# - generating  build
+# - commit changes with release tag
+
+import argparse
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
