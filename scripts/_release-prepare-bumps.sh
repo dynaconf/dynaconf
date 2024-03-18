f483f4b268015bee55cc20fd58af4c60e14b24fe
Pedro Brochado <pedropsb95@gmail.com>
2024-03-18 17:11:13 -0300

    misc: fix changelog generation order

--- scripts/_release-prepare-bumps.sh
+++ scripts/_release-prepare-bumps.sh
@@ -6,14 +6,14 @@
 
 set -euo pipefail
 
-# update changelog in-place with current_version (e.g: 3.2.5-dev0)
-echo "[BUMP] Updating changelog"
-CURRENT_VERSION="$(bump-my-version show current_version)"
-git-changelog --bump "${CURRENT_VERSION:?is-empty}"
-
 # {major}.{minor}.{patch}-{pre}.{pre_n}
 # e.g: 3.2.5-dev0 -> 3.2.5
 echo "[BUMP] Bumping files for distribution version: x.y.z-dev0 -> x.y.z"
 bump-my-version bump pre
+
+# update changelog in-place with current_version (e.g: 3.2.5-dev0)
+echo "[BUMP] Updating changelog"
+CURRENT_VERSION="$(bump-my-version show current_version)"
+git-changelog --bump "${CURRENT_VERSION:?is-empty}"
 
 echo "[BUMP] Done."
