48a7ad41209d8d4b8cc25650c592c2fc0f5f6e69
Bruno Rocha <rochacbruno@users.noreply.github.com>
2024-07-04 13:56:47 +0100

    misc(django): Add test for settings override with context manager (#1122)
    closes #689

--- tests_functional/django_pytest/app/tests/test_app.py
+++ tests_functional/django_pytest/app/tests/test_app.py
@@ -7,6 +7,7 @@ django.setup()  # noqa
 import pytest
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.test.utils import override_settings
 
 
 @pytest.mark.django_db
@@ -15,3 +16,16 @@ def test_admin_user():
 
     assert User.objects.filter(username="admin").exists()
     assert settings.FOO == "foo-test"
+
+
+@override_settings(CONTENT_APP_TTL=30)
+def test_settings_override_689():
+    # before
+    assert settings.CONTENT_APP_TTL == 30
+
+    # after
+    with override_settings(CONTENT_APP_TTL=1):
+        assert settings.CONTENT_APP_TTL == 1
+
+    # same as before
+    assert settings.CONTENT_APP_TTL == 30
