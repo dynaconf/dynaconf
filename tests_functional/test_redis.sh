20ee605f50917cfbff13e84465649a35fd4cd17d
Bruno Rocha <rochacbruno@users.noreply.github.com>
2024-07-10 13:39:39 +0100

    misc(tests):Reorganized functional tests (#1153)

--- tests_functional/test_redis.sh
+++ tests_functional/test_redis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run --rm --name dynaconf_with_redis -d -p 6379:6379 redis:alpine || true
 sleep 2
-cd tests_functional/redis_example
+cd tests_functional/legacy/redis_example
 pwd
 dynaconf -i dynaconf.settings write redis -s FOO=foo_is_default
 dynaconf -i dynaconf.settings write redis -s SECRET=redis_works_in_default
@@ -10,4 +10,4 @@ dynaconf -i dynaconf.settings write redis -e production -s SECRET=redis_works_in
 sleep 2
 python redis_example.py
 docker stop dynaconf_with_redis || true
-cd ../../
+cd ../../../
