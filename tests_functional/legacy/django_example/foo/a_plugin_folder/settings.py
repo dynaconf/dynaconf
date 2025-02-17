f9f540d6954c356238caf6419589b5fae78f958e
Bruno Rocha <rochacbruno@users.noreply.github.com>
2025-02-17 15:13:46 +0000

    fix: Hotfix hook collector to avoid eager evaluation. (#1255) (#1256)
    Using getattr during load on a value would trigger
    eager evaluation, changed to use name instead.

--- tests_functional/legacy/django_example/foo/a_plugin_folder/settings.py
+++ tests_functional/legacy/django_example/foo/a_plugin_folder/settings.py
@@ -1,3 +1,13 @@
 from __future__ import annotations
 
 BANDS = ["Metallica", "Black Sabbath", "Iron Maiden"]
+
+
+# This is really bad, however there are some running projects using it.
+# namely pulp_ansible <0.24
+from dynaconf import settings
+
+# this must be avoided in favor of lazy format, get a and hooks
+BAD = settings.BEST_BOSS + "/foo"
+# the following is ok
+GOOD = "@format {this.TEST_VALUE}/foo"
