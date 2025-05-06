f8189fb6c130ff4709450e7daebcf1c174aea248
Bruno Rocha <rochacbruno@gmail.com>
2025-05-06 16:37:29 +0100

    fix: add correct supported python version to 3.2.x

--- setup.py
+++ setup.py
@@ -94,6 +94,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Utilities",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
