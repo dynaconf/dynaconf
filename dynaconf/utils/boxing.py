7664ea5ab0e18cddfc5c7254511ac0b19e372e04
Pedro Brochado <pedropsb95@gmail.com>
2025-07-04 10:51:56 -0300

    refactor: rewrite decorator as explicit call
    The decorator makes it a bit hard to understand what's going on.
    
    Also, it improved the access performance by ~2x by accident [0],
    although we don't yet have cache.
    
    [0]: The test used:
    
    ```
    from dynaconf import Dynaconf
    data = {"common": {"mode": 123}}
    settings = Dynaconf(**data)
    for _ in range(40_000):
        settings.common.mode
    ```

--- dynaconf/utils/boxing.py
+++ dynaconf/utils/boxing.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import inspect
-from functools import wraps
 
 from dynaconf.utils import find_the_correct_casing
 from dynaconf.utils import recursively_evaluate_lazy_format
@@ -9,44 +8,35 @@ from dynaconf.utils.functional import empty
 from dynaconf.vendor.box import Box
 
 
-def evaluate_lazy_format(f):
-    """Marks a method on Dynabox instance to
-    lazily evaluate LazyFormat objects upon access."""
-
-    @wraps(f)
-    def evaluate(dynabox, item, *args, **kwargs):
-        value = f(dynabox, item, *args, **kwargs)
-        settings = dynabox._box_config["box_settings"]
-
-        if getattr(value, "_dynaconf_lazy_format", None):
-            dynabox._box_config[f"__raw_{item.lower()}"] = (
-                f"@{value.formatter.token} {value.value}"
-            )
-
-        return recursively_evaluate_lazy_format(value, settings)
-
-    return evaluate
-
-
 class DynaBox(Box):
     """Specialized Box for dynaconf
     it allows items/attrs to be found both in upper or lower case"""
 
-    @evaluate_lazy_format
     def __getattr__(self, item, *args, **kwargs):
         try:
-            return super().__getattr__(item, *args, **kwargs)
+            result = super().__getattr__(item, *args, **kwargs)
         except (AttributeError, KeyError):
             n_item = find_the_correct_casing(item, self) or item
-            return super().__getattr__(n_item, *args, **kwargs)
+            result = super().__getattr__(n_item, *args, **kwargs)
+        return self.__evaluate_lazy__(result)
 
-    @evaluate_lazy_format
     def __getitem__(self, item, *args, **kwargs):
         try:
-            return super().__getitem__(item, *args, **kwargs)
+            result = super().__getitem__(item, *args, **kwargs)
         except (AttributeError, KeyError):
             n_item = find_the_correct_casing(item, self) or item
-            return super().__getitem__(n_item, *args, **kwargs)
+            result = super().__getitem__(n_item, *args, **kwargs)
+        return self.__evaluate_lazy__(result)
+
+    def get(self, item, default=None, *args, **kwargs):
+        n_item = find_the_correct_casing(item, self) or item
+        result = super().get(n_item, empty, *args, **kwargs)
+        result = result if result is not empty else default
+        return self.__evaluate_lazy__(result)
+
+    def __evaluate_lazy__(self, result):
+        settings = self._box_config["box_settings"]
+        return recursively_evaluate_lazy_format(result, settings)
 
     def _safe_get(self, item, *args, **kwargs):
         """Get item bypassing recursive evaluation"""
@@ -74,12 +64,6 @@ class DynaBox(Box):
             super(Box, self).copy(),
             box_settings=self._box_config.get("box_settings"),
         )
-
-    @evaluate_lazy_format
-    def get(self, item, default=None, *args, **kwargs):
-        n_item = find_the_correct_casing(item, self) or item
-        value = super().get(n_item, empty, *args, **kwargs)
-        return value if value is not empty else default
 
     def __dir__(self):
         keys = list(self.keys())
